622c2e0533a593a15f3dd7096a2b0d0b62935b3d
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-10-27 23:02:02 -0300

    Create section for Available Meals

---
+++ src/components/AvailableMeals/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import style from './styles.module.scss';
+import { Card } from '../UI';
+import { MealsItem } from '../MealsItem';
+
+
+export const AvailableMeals = ({ mealsList }) => {
+  console.log(mealsList)
+  return (
+    <section className={style.meals}>
+      <Card>
+        <ul>
+          {mealsList.map(meal => <MealsItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price} />)}
+        </ul>
+      </Card>
+    </section>
+  );
+};
