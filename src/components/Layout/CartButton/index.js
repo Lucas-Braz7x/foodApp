52199de066c51aa3a673509f13736edef975a103
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-11-03 21:37:11 -0300

    Add animation in the header button

--- src/components/Layout/CartButton/index.js
+++ src/components/Layout/CartButton/index.js
@@ -1,13 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../../Cart/CartIcon';
 import CartContext from '../../../store/cart-context';
 import style from './styles.module.scss';
 
 export const CartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
+  const { items } = cartCtx;
+  const btnClasses = `${style.button} ${btnIsHighlighted ? style.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+
+  }, [items]);
+
   return (
-    <button className={`${style.button} ${style.bump}`} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={style.icon}><CartIcon /></span>
       <span>Seu carrinho</span>
       <span className={style.badge}>
