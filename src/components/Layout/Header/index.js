801dd99925661de11bb4f5feafc3d10edb4accae
Lucas Braz Dutra <55291250+Lucas-Braz7x@users.noreply.github.com>
2021-11-16 20:57:28 -0300

    Update index.js

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import style from './styles.module.scss';
-import 'animate.css';
 import mealsImg from '../../../assets/meals-small.jpg';
 import { CartButton } from '../CartButton';
 
